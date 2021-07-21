c0c04bfff369a7dc328279146503dc4ff1c31f21
EvgenStr <juve2910@gmail.com>
2021-07-21 13:18:37 +0300

    add migration and model

---
+++ migrations/20210721084801-create-user.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        field: 'first_name',
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        field: 'last_name',
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.TEXT,
+      },
+      birthday: {
+        type: Sequelize.DATEONLY,
+      },
+      isMale: {
+        field: 'is_male',
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        field: 'created_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        field: 'updated_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  },
+};
