2c7fd6a852c3a4a0e3291d06c680f134c51e0d94
EvgenStr <juve2910@gmail.com>
2021-07-25 21:56:44 +0300

    add routes for tasks

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -28,3 +28,78 @@ module.exports.getUserTasks = async (req, res, next) => {
     next(err);
   }
 };
+
+module.exports.getAllTasks = async (req, res, next) => {
+  try {
+    const [rowsCount, tasks] = await Task.findAndCountAll();
+    if (rowsCount === 0) {
+      const err = createError(404, 'Users not found');
+      return next(err);
+    }
+    res.status(200).send({
+      data: tasks,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getTask = async (req, res, next) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+
+    const task = await User.findByPk(id);
+    if (!task) {
+      const err = createError(404, 'Task not found');
+      return next(err);
+    }
+    res.send(task);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updateTask = async (req, res, next) => {
+  try {
+    const {
+      params: { id },
+      body,
+    } = req;
+
+    const [rowsCount, updatedTask] = await Task.update(body, {
+      where: { id },
+      returning: true,
+    });
+    if (rowsCount !== 1) {
+      return next(createError(400, "Task can't be updated"));
+    }
+
+    res.send({
+      data: updatedTask,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deleteTask = async (req, res, next) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+
+    const rowsCount = await Task.destroy({
+      where: { id },
+    });
+
+    if (rowsCount !== 1) {
+      return next(createError(404, 'Task not found'));
+    }
+
+    res.send({ data: rowsCount });
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/task.js
+++ routes/task.js
@@ -5,6 +5,9 @@ const { checkUser } = require('../middlewares/user.mw');
 const taskRouter = Router();
 
 taskRouter.post('/:id', checkUser, TaskController.createTask);
-taskRouter.get('/:id', checkUser, TaskController.getUserTasks);
+taskRouter.get('/:id/user', checkUser, TaskController.getUserTasks);
+taskRouter.get('/:id', TaskController.getTask);
+taskRouter.patch('/:id', TaskController.updateTask);
+taskRouter.delete('/:id', TaskController.deleteTask);
 
 module.exports = taskRouter;
